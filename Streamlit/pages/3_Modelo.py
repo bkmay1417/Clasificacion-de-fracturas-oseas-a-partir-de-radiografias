6e5715951db824ebfe550bc9224264cee6e9f962
Michael Martinez <163685041+bkmay1417@users.noreply.github.com>
2024-12-26 19:10:05 -0300

    Update 3_Modelo.py

--- Streamlit/pages/3_Modelo.py
+++ Streamlit/pages/3_Modelo.py
@@ -10,7 +10,7 @@ from functools import lru_cache
 import cv2
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Obtiene el directorio base
-SAVED_MODEL_DIR = os.path.join(BASE_DIR, "modeloexportado", "saved_model")
+SAVED_MODEL_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "modeloexportado", "saved_model"))
 MODEL_ZIP = "modeloexportado.zip"
 
 if os.path.exists(SAVED_MODEL_DIR):
