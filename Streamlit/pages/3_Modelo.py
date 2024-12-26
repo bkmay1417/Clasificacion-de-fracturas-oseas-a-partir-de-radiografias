bc7e68a8557c4e34d4614170d5d91cdf7ed2a02b
Michael Martinez <163685041+bkmay1417@users.noreply.github.com>
2024-12-26 18:13:57 -0300

    Update 3_Modelo.py

--- Streamlit/pages/3_Modelo.py
+++ Streamlit/pages/3_Modelo.py
@@ -9,9 +9,6 @@ from PIL import Image
 from functools import lru_cache
 import cv2
 
-# Ruta al directorio actual (steamlit/pages)
-current_dir = os.path.dirname(__file__)
-
 # Ruta al modelo, relativa al directorio actual
 SAVED_MODEL_DIR = "/workspaces/Clasificacion-de-fracturas-oseas-a-partir-de-radiografias/modeloexportado/saved_model"
 MODEL_ZIP = "modeloexportado.zip"
